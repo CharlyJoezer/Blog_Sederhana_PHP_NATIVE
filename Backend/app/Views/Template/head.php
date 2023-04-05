31ee8a9cbb78ae271f0644bac62416f221878f75
CharlyJoezer <charlyjoezer30@gmail.com>
2023-04-05 20:17:46 +0800

    Mengubah letak script cdn dari footer ke head

--- Backend/app/Views/Template/head.php
+++ Backend/app/Views/Template/head.php
@@ -6,6 +6,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/<?= $data['css']?>.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script
+    src="https://code.jquery.com/jquery-3.6.4.js"
+    integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
+    crossorigin="anonymous"></script>
     <title><?= $data['title']; ?></title>
     <style>
       .navbar{
